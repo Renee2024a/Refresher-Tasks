0b3a739368844986b87d763cb9c087dd9c9dd6e3
Renee2024a <ctw19.renee.lapierre@corketb.ie>
2024-09-11 12:07:53 +0100

    Add files via upload

---
+++ Magic 8 ball.py
@@ -0,0 +1,13 @@
+import random
+1 = "you can do it"
+2 = "take a chance"
+3 = "yes"  
+4 = "no"
+
+print int(intpu(("Welcome to the magic 8 ball")))
+Question=int(input())
+
+print("Shaking")
+choice = random.randint(1,8)
+
+print(choice) 
